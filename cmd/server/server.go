954274d458526f98317aff8699be53491a38a674
SamudiHansani <samudi.hansani@ifs.com>
2026-02-01 11:56:31 +0530

    changed directory

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -7,5 +7,5 @@ import (
 
 func main() {
 	log.Println("Serving at http://localhost:8080")
-	http.ListenAndServe(":8080", http.FileServer(http.Dir("../../")))
+	http.ListenAndServe(":8080", http.FileServer(http.Dir("./")))
 }
